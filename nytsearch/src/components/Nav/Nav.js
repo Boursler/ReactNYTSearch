5c871591a4e1d8112dba6b2e34efe7d8acdc4e1f
Briana Oursler <briana.oursler@gmail.com>
2018-06-23 17:14:17 -0700

    More components for the front end appearance

---
+++ nytsearch/src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => (
+  <nav className="navbar navbar-expand-lg navbar-secondary bg-primary">
+    <a className="navbar-brand" href="/">
+      React Reading List
+    </a>
+  </nav>
+);
+
+export default Nav;
