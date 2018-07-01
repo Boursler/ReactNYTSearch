ed4f7a89b16eff4214e895f68ce6c87fcae0b63e
Briana Oursler <briana.oursler@gmail.com>
2018-07-01 12:30:16 -0700

    Update router references

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const routes = require("./server/routes/articles");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -20,5 +20,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log(`🌎  ==> API Server on PORT ${PORT}!`);
 });
