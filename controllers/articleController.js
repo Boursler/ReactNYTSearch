c800c85401584e129ce5d00029393aac4953de52
Briana Oursler <briana.oursler@gmail.com>
2018-07-01 14:03:52 -0700

    Add proxy so API calls are successful

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -6,6 +6,7 @@ module.exports = {
     db.Article
       .find(req.query)
       .sort({ date: -1 })
+   
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
