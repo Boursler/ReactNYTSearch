5b131f88fd53b4d3746831b8e907e44b5864ec82
Briana Oursler <briana.oursler@gmail.com>
2018-07-14 17:34:37 -0700

    Fix bug missing / in call to delete

--- nytsearch/src/utils/Api.js
+++ nytsearch/src/utils/Api.js
@@ -5,10 +5,11 @@ export default{
 		return axios.get(queryURL);
 	},
 	save: function(articleData){
+		// console.log("calling this function to save");
 		return axios.post("/api/articles", articleData)
 	}, 
 	delete: function(id){
-		return axios.delete("/api/articles" + id);
+		return axios.delete("/api/articles/" + id);
 	}, 
 	getSaved: function(){
 		return axios.get("/api/articles");
