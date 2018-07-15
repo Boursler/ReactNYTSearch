a4bf26739aae6c44f380f5fa59a3679e2dda8717
Briana Oursler <briana.oursler@gmail.com>
2018-07-14 17:11:33 -0700

    Add Moment to format pub_date on Saved

--- nytsearch/src/components/Saved.js
+++ nytsearch/src/components/Saved.js
@@ -1,11 +1,12 @@
 import React from "react";
 import DeleteBtn from "./DeleteBtn";
 import {Col} from "./Grid";
+import Moment from "moment";
 // import API from "../utils/Api";
- 
+//  <p>Publication Date: {Moment(article.pub_date).format('MMM Do YY')}</p>
 const Saved = props => (
 	<Col size="md-6">
-	<p>{props.date}</p>
+	<p>{Moment(props.date).format('MMM Do YY')}</p>
 	<a href={props.url} target="_blank">{props.title}</a>
 	<DeleteBtn onClick={() => this.deleteArticle(props._id)}/>
 	</Col>
